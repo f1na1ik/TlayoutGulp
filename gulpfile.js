25ae1f9a7593d90b2104480b2d992711c6d1f8f8
f1na1ik <maxsim119@yandex.ru>
2019-08-06 20:10:23 +0300

    podkluchil slick slider, udalyat' .scss v slick-carousel

--- gulpfile.js
+++ gulpfile.js
@@ -33,7 +33,7 @@ var gulp = require('gulp'),
 gulp.task('js',function () {
     return gulp.src(['node_modules/jquery/dist/jquery.min.js',
         'node_modules/bootstrap/dist/js/bootstrap.min.js',
-        'node_modules/slick-carousel/slick/slick.js'
+        'node_modules/slick-carousel/slick/slick.min.js'
         // 'node_modules/magnific-popup/dist/jquery.magnific-popup.js'
     ])
         .pipe(concat('libs.min.js'))
